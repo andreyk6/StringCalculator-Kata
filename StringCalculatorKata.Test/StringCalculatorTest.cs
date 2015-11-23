91bad420f10a8fd61070f7c7f13a053abd6e1c18
andreyk6 <andreyk6@gmail.com>
2015-11-23 18:51:12 +0200

    Final version of the Kata

---
+++ StringCalculatorKata.Test/StringCalculatorTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StringCalculatorKata.Test
+{
+
+    [TestFixture]
+    public class StringCalculatorTest
+    {
+        [Test]
+        public void Add_WhenEmptyString_Returns0()
+        {
+            
+            ArrangeActAndAssert("", 0);
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        public void Add_WhenSingleNumber_ReturnNumber(string numbers, int excepted)
+        {
+            ArrangeActAndAssert(numbers, excepted);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("2,3", 5)]
+        [TestCase("10,11", 21)]
+        public void Add_WhenMultipleNumbers_ReturnSumOfNumbers(string numbers, int excepted)
+        {
+            ArrangeActAndAssert(numbers, excepted);
+        }
+
+        [TestCase("1,2,3", 6)]
+        [TestCase("1,2,3,4", 10)]
+        public void Add_WhenUnknownAmountOfNumbers_ReturnSumOfNumbers(string numbers, int excepted)
+        {
+            ArrangeActAndAssert(numbers, excepted);
+        }
+
+        public void Add_EmptyStringWithNewLine_Returns0(string numbers, int excepted)
+        {
+            ArrangeActAndAssert("\n", 0);
+        }
+
+        [TestCase("1\n2,3\n4", 10)]
+        [TestCase("5,5\n5\n5", 20)]
+        public void Add_WhenNumbersSplitWithNewLine_ReturnSumOfNumbers(string numbers, int excepted)
+        {
+            ArrangeActAndAssert(numbers, excepted);
+        }
+
+        [TestCase("//[;]\n1;2;3;4", 10)]
+        [TestCase("//[&]\n3&4&5&6", 18)]
+        public void Add_WithCustomSeparator_ReturnSumOfNumbers(string numbers, int excepted)
+        {
+            ArrangeActAndAssert(numbers, excepted);
+        }
+
+        [TestCase("//[;;]\n1;;2;;3;;4", 10)]
+        [TestCase("//[++]\n3++4++5++6", 18)]
+        public void Add_WithCustomLongSeparator_ReturnSumOfNumbers(string numbers, int excepted)
+        {
+            ArrangeActAndAssert(numbers, excepted);
+        }
+
+        [TestCase("//[;][+]\n1;2+3;4", 10)]
+        [TestCase("//[+][;][&]\n3+4&5;6", 18)]
+        public void Add_WithMultipleSeparators_ReturnSumOfNumbers(string numbers, int excepted)
+        {
+            ArrangeActAndAssert(numbers, excepted);
+        }
+
+        [Test]
+        public void Add_ThrowsOnNegativeArgs()
+        {
+            var testDelegate = new TestDelegate(() => ArrangeActAndAssert("1,-1,2,3", 0));
+            //Catch exception
+            Assert.Catch(testDelegate, "Negative args");
+        }
+
+        [TestCase("1000,10,30,50",90)]
+        public void Add_WithNumbersMoreThan1000_ReturnSumWithIgnoredNumbersMoreThan1000(string numbers, int excepted)
+        {
+            ArrangeActAndAssert(numbers, excepted);
+        }
+
+        private static void ArrangeActAndAssert(string numbers, int excepted)
+        {
+            StringCalculator sCalc = new StringCalculator();
+            var result = sCalc.Add(numbers);
+            Assert.AreEqual(result, excepted);
+        }
+    }
+}
---
+++ StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        private const int DefaultValue = 0;
+        private const char DefaultSeparator = ',';
+
+        public int Add(string numbers)
+        {
+            numbers = SanitizeNumbers(numbers);
+            if (ShouldReturnDefaultNumber(numbers))
+                return DefaultValue;
+            if (ShouldConvertMultipleNumbers(numbers))
+                return ConvertMultipleNumbers(numbers);
+
+            return ConvertSingleNumber(numbers);
+        }
+
+        private static string SanitizeNumbers(string numbers)
+        {
+            //Add replace custom separator with default support
+            if (CustomSeparatorExist(numbers))
+            {
+                numbers = ReplaceCustomSeparators(numbers);
+            }
+
+            return numbers.Replace('\n', DefaultSeparator);
+        }
+
+        private static string ReplaceCustomSeparators(string numbers)
+        {
+            //Get custom separator
+            string[] separators = GetCustomSeparator(numbers);
+            numbers = RemoveSeparator(numbers);
+            foreach (var separator in separators)
+            {
+                numbers = numbers.Replace(separator, DefaultSeparator.ToString());
+            }
+            return numbers;
+        }
+
+        private static string RemoveSeparator(string numbers)
+        {
+            int separatorEnd = numbers.IndexOf("]\n");
+            numbers = numbers.Substring(separatorEnd + 2);
+            return numbers;
+        }
+
+        private static string[] GetCustomSeparator(string numbers)
+        {
+            int separatorsEnd = numbers.IndexOf("]\n") - 1;
+            string separatorsStr = numbers.Substring(2, separatorsEnd);
+
+            List<string> separators = new List<string>();
+
+            while (separatorsStr.Length > 0)
+            {
+                int separatorEnd = separatorsStr.IndexOf(']');
+                separators.Add(separatorsStr.Substring(1, separatorEnd - 1));
+                separatorsStr = separatorsStr.Substring(separatorEnd + 1);
+            }
+
+            return separators.ToArray();
+        }
+
+        private static bool CustomSeparatorExist(string numbers)
+        {
+            return numbers.StartsWith("//");
+        }
+
+        private static int ConvertMultipleNumbers(string numbers)
+        {
+            var splitedNumbers = numbers.Split(DefaultSeparator);
+            return splitedNumbers.Sum(x => ConvertSingleNumber(x)); 
+        }
+
+        private static bool ShouldConvertMultipleNumbers(string numbers)
+        {
+            if (numbers.Contains(DefaultSeparator))
+                return true;
+            else
+                return false;
+        }
+
+        private static int ConvertSingleNumber(string numbers)
+        {
+            int result = Int32.Parse(numbers);
+
+            if (result >= 1000) result = 0;
+
+            if (result < 0) throw new ArgumentOutOfRangeException("Negative args");
+
+            return result;
+        }
+
+        private static bool ShouldReturnDefaultNumber(string numbers)
+        {
+            return String.IsNullOrEmpty(numbers);
+        }
+    }
+}
